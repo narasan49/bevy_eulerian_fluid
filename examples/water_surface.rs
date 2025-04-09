19b4e71927e49227c028a37a7a1c16d08ee9760f
narasan49 <narasan@hotmail.co.jp>
2025-04-09 22:19:30 +0900

    feat: temporal interval 0.5 -> 0.1

--- examples/water_surface.rs
+++ examples/water_surface.rs
@@ -70,7 +70,7 @@ fn setup_scene(mut commands: Commands) {
 
     commands.spawn(FluidSettings {
         dx: 1.0f32,
-        dt: 0.5f32,
+        dt: 0.1f32,
         rho: 997f32, // water
         gravity: Vec2::Y,
         size: SIZE,
