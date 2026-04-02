07ff011aec31e7635fd55996063cee5bb4e3d40d
Yusuke Nara <narasan49@gmail.com>
2026-04-02 18:09:12 +0900

    feat: count frames

--- crates/example_utils/src/fps_counter.rs
+++ crates/example_utils/src/fps_counter.rs
@@ -9,6 +9,9 @@ pub struct FpsRoot;
 #[derive(Component)]
 pub struct FpsText;
 
+#[derive(Component)]
+pub struct FrameCountText;
+
 /// FPS counter plugin comming from Unofficial Bevy Cheat Book,
 /// https://bevy-cheatbook.github.io/cookbook/print-framerate.html
 pub struct FpsCounterPlugin;
@@ -17,7 +20,7 @@ impl Plugin for FpsCounterPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(FrameTimeDiagnosticsPlugin::default())
             .add_systems(Startup, setup_fps_text)
-            .add_systems(Update, update_fps_text);
+            .add_systems(Update, (update_fps_text, update_frame_count_text));
     }
 }
 
@@ -27,6 +30,7 @@ fn setup_fps_text(mut commands: Commands) {
             FpsRoot,
             Node {
                 position_type: PositionType::Absolute,
+                flex_direction: FlexDirection::Column,
                 right: Val::Percent(1.0),
                 top: Val::Percent(1.0),
                 bottom: Val::Auto,
@@ -55,7 +59,29 @@ fn setup_fps_text(mut commands: Commands) {
             FpsText,
         ))
         .id();
-    commands.entity(root).add_child(text);
+
+    let frane_count_text = commands
+        .spawn((
+            Text::new("Frame Count: "),
+            TextFont {
+                font_size: 16.0,
+                ..default()
+            },
+            TextColor::WHITE,
+        ))
+        .with_child((
+            TextSpan::default(),
+            TextFont {
+                font_size: 16.0,
+                ..default()
+            },
+            TextColor::WHITE,
+            FrameCountText,
+        ))
+        .id();
+    commands
+        .entity(root)
+        .add_children(&[text, frane_count_text]);
 }
 
 fn update_fps_text(
@@ -65,6 +91,22 @@ fn update_fps_text(
     for mut text in &mut query {
         if let Some(fps) = diagnostics
             .get(&FrameTimeDiagnosticsPlugin::FPS)
+            .and_then(|fps| fps.smoothed())
+        {
+            **text = format!("{fps:>4.0}");
+        } else {
+            **text = "N/A".into();
+        }
+    }
+}
+
+fn update_frame_count_text(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut TextSpan, With<FrameCountText>>,
+) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics
+            .get(&FrameTimeDiagnosticsPlugin::FRAME_COUNT)
             .and_then(|fps| fps.smoothed())
         {
             **text = format!("{fps:>4.0}");
