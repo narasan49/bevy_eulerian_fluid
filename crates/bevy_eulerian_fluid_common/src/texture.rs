0e5dc3419968049516841eb78c009fd639aa9dfe
Yusuke Nara <narasan49@gmail.com>
2026-05-30 23:25:18 +0900

    feat: add new_texture_storage_2d helper

--- crates/bevy_eulerian_fluid_common/src/texture.rs
+++ crates/bevy_eulerian_fluid_common/src/texture.rs
@@ -31,3 +31,30 @@ pub fn new_texture_storage_3d(
 
     images.add(image)
 }
+
+pub fn new_texture_storage_2d(
+    images: &mut Assets<Image>,
+    size: UVec2,
+    format: TextureFormat,
+) -> Handle<Image> {
+    let pixel_size = format.pixel_size().unwrap();
+    let mut zeros = Vec::new();
+    zeros.resize(pixel_size, 0u8);
+
+    let mut image = Image::new_fill(
+        Extent3d {
+            width: size.x,
+            height: size.y,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &zeros,
+        format,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+
+    image.texture_descriptor.usage =
+        TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
+
+    images.add(image)
+}
